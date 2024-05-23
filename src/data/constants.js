1e253ff85e3db3b051bbbc8268c8c4efccbb3154
VYAMASANI <114718456+IASNAVAP@users.noreply.github.com>
2024-05-23 22:28:04 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/IASNAVAP",
   resume:
-    "https://drive.google.com/file/d/1zHMO8wADIaIx8hpnPOGvn4NtcPEAhU4O/view?usp=sharing",
+    "https://drive.google.com/file/d/1HVnSVLMMW3kLRW3Xqbjzxy7TmlFAVXeT/view",
   linkedin: "HTTPS://www.linkedin.com/in/vyamasani-pavan-sai-841339261/",
   twitter: "https://x.com/VyamasaniS23083",
   insta: "https://www.instagram.com/v_p_s_22/",
